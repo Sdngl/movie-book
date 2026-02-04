201656ce7fe702ede98686cab827e682f634bf60
Sdngl <sdngl12345@gmail.com>
2026-02-04 11:20:03 +0545

    git push with .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,16 +6,17 @@ import { getAnalytics } from "firebase/analytics";
 // Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "movie-booking-s.firebaseapp.com",
-  projectId: "movie-booking-s",
-  storageBucket: "movie-booking-s.appspot.com",
-  messagingSenderId: "633334302714",
-  appId: "1:633334302714:web:27a6903b2148fff2abc643",
-  measurementId: "G-0BTFH87WP8"
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
 
 // Optional analytics
 export const analytics = getAnalytics(app);
